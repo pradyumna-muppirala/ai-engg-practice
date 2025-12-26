5b94b78bff41b2137d03dc4854b923d7efbd1776
pradyumna-muppirala <pradyumna.muppirala@gmail.com>
2025-12-26 13:20:49 +0530

    Adding 1st Jupyter notebook sample and python language exercises py file

---
+++ PythonLang-Practice.py
@@ -0,0 +1,51 @@
+#Exercise 1
+print("----- Exercise 1 -----" )  
+#Integer and floats
+
+age=20
+height = 5.5
+
+#Strings
+name ="Alice"
+greetings = "Hello " + name
+
+#Lists and Tuples
+numbers = [2,5,7,9]
+names = ["Alice", "Bob", "Charlie"]
+Coodinates = (34, 45)
+
+#Dictionaries
+person = { name: "Shiva", age:0 , height: 5.6}
+
+#Booleans
+is_human = False
+
+print (age)
+print (height)
+print (name)
+print (greetings)
+print (numbers)
+print (names)
+print (Coodinates)
+print (person)
+print (is_human)
+
+#Exercise 2
+print("----- Exercise 2 -----" )    
+#Define variables of different data types
+
+Int_variable = 20
+float_variable = 3.14
+string_var = "AI"
+list_var = [1, 2, 3, 4, 5]
+tuple_var = (10, 20, 30)
+dict_var = {"name": "Alice", "role": "AI engineer" }
+bool_var = True
+print(Int_variable)
+print(float_variable)
+print(string_var + " Bootcamp")
+list_var.append(6)
+print(list_var)
+print(tuple_var)
+print(dict_var)
+print(bool_var)
