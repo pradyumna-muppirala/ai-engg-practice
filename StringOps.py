bc7d2fa948c12d1ad7f5d724558bdd4cc8c778a4
pradyumna-muppirala <pradyumna.muppirala@gmail.com>
2025-12-26 18:37:56 +0530

    String Ops and Reg Ex exercises

--- StringOps.py
+++ StringOps.py
@@ -21,3 +21,40 @@ def is_palindrome(s):
     """Checks if the input string is a palindrome."""
     cleaned = ''.join(char.lower() for char in s if char.isalnum())
     return cleaned == cleaned[::-1]
+
+first="Hello"
+second="World"
+
+result = first + " " + second
+print(result)  # Output: Hello World
+
+def concat_strings(s1, s2):
+    """Concatenates two strings."""
+    return s1 + s2
+
+text ="Python Programming"
+
+print(text[0:6])    # Output: Python
+print(text[-11:])   # Output: Programming
+
+name = "Alice"
+age = 25
+info = f"My name is {name} and I am {age} years old."
+print(info)  # Output: My name is Alice and I am 25 years old.
+
+sentence = "The quick brown fox jumps over the lazy dog"
+words = sentence.split()
+print(words)  # Output: ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'] 
+
+joined_sentence = ' '.join(words)
+print(joined_sentence)  # Output: The quick brown fox jumps over the lazy dog   
+
+joined_sentence = "|".join(words)
+print(joined_sentence)  # Output: The|quick|brown|fox|jumps|over|the|lazy|dog
+
+sentence = sentence.replace("fox", "wolf")
+print(sentence)  # Output: The quick brown fox jumps over the lazy dog
+
+original = "  Hello, World!  "
+trimmed = original.strip()  
+print(f"'{trimmed}'")  # Output: 'Hello, World!'
